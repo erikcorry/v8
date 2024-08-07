48f161a553aa5530df67d3968153ebeb3d1e308d
Dmitry Bezhetskov <dima00782@gmail.com>
2024-08-07 16:11:10 +0200

    Initialize Sandbox for SweepingJob::Run

--- src/heap/array-buffer-sweeper.cc
+++ src/heap/array-buffer-sweeper.cc
@@ -196,6 +196,7 @@ class ArrayBufferSweeper::SweepingState::SweepingJob final : public JobTask {
 
 void ArrayBufferSweeper::SweepingState::SweepingJob::Run(
     JobDelegate* delegate) {
+  PtrComprCageAccessScope ptr_compr_cage_access_scope(heap_->isolate());
   const ThreadKind thread_kind =
       delegate->IsJoiningThread() ? ThreadKind::kMain : ThreadKind::kBackground;
   if (treat_all_young_as_promoted_ == TreatAllYoungAsPromoted::kNo) {
