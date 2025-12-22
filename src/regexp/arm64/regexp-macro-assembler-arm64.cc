c93f78c6d5ced22b46304868e59a2ef06fb3bf72
Erik Corry <erikcorry@chromium.org>
2025-12-22 11:54:14 +0100

    Fix SIMD heuristic

--- src/regexp/arm64/regexp-macro-assembler-arm64.cc
+++ src/regexp/arm64/regexp-macro-assembler-arm64.cc
@@ -764,8 +764,8 @@ void RegExpMacroAssemblerARM64::SkipUntilBitInTable(
     Label* on_no_match) {
   Label scalar_repeat;
 
-  if (SkipUntilBitInTableUseSimd(advance_by)) {
-    DCHECK(!nibble_table_array.is_null());
+  if (!nibble_table_array.is_null()) {
+    // Use SIMD.
     Label scalar;
     EmitSkipUntilBitInTableSimdHelper(
         cp_offset, advance_by, nibble_table_array, 0, &scalar,
