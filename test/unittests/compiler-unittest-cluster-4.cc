7d168ef61bd00a303ffa436f9388b7cd61bdced3
Erik Corry <erikcorry@chromium.org>
2026-02-08 20:40:16 +0100

    Compiler unit tests

---
+++ test/unittests/compiler-unittest-cluster-4.cc
@@ -0,0 +1,14 @@
+// Copyright 2026 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Cluster build file for test/unittests/compiler (linkage/load/loop/machine)
+
+#pragma clang diagnostic ignored "-Wheader-hygiene"
+
+#include "compiler/linkage-tail-call-unittest.cc"
+#include "compiler/linkage-unittest.cc"
+#include "compiler/load-elimination-unittest.cc"
+#include "compiler/loop-peeling-unittest.cc"
+#include "compiler/machine-operator-reducer-unittest.cc"
+#include "compiler/machine-operator-unittest.cc"
---
+++ test/unittests/compiler-unittest-cluster-7.cc
@@ -0,0 +1,21 @@
+// Copyright 2026 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Cluster build file for test/unittests/compiler (scheduler/simplified/types)
+
+#pragma clang diagnostic ignored "-Wheader-hygiene"
+
+#include "compiler/schedule-unittest.cc"
+#include "compiler/scheduler-rpo-unittest.cc"
+#include "compiler/scheduler-unittest.cc"
+#include "compiler/simplified-lowering-unittest.cc"
+#include "compiler/simplified-operator-reducer-unittest.cc"
+#include "compiler/simplified-operator-unittest.cc"
+#include "compiler/sloppy-equality-unittest.cc"
+#include "compiler/state-values-utils-unittest.cc"
+#include "compiler/typed-optimization-unittest.cc"
+#include "compiler/typer-unittest.cc"
+#include "compiler/types-unittest.cc"
+#include "compiler/value-numbering-reducer-unittest.cc"
+#include "compiler/zone-stats-unittest.cc"
