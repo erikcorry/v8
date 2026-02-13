75c1bf14fe52605464b48bbf932a09af8a87f614
Erik Corry <erikcorry@chromium.org>
2026-02-13 17:41:08 +0100

    Undo exclusion no longer needed

--- test/cctest/test-api.h
+++ test/cctest/test-api.h
@@ -31,7 +31,7 @@ static void CheckReturnValueImpl(v8::Isolate* v8_isolate,
   CHECK_EQ(v8_isolate, return_value.GetIsolate());
   i::Isolate* isolate = reinterpret_cast<i::Isolate*>(v8_isolate);
 
-  Tagged<Object> default_value;
+  Tagged<i::Object> default_value;
   if constexpr (is_int) {
     default_value = Smi::zero();
   } else if constexpr (is_bool) {
@@ -45,7 +45,7 @@ static void CheckReturnValueImpl(v8::Isolate* v8_isolate,
   }
 
   auto CheckValueMap = [=](v8::ReturnValue<T>& return_value) {
-    Tagged<Object> obj = Get(return_value);
+    Tagged<i::Object> obj = Get(return_value);
     if constexpr (is_int) {
       return IsNumber(obj);
     } else if constexpr (is_bool) {
